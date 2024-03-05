4dfabf321e611c5eb8bd51d1e88667632d5193e6
jorge vergara <jorgevergarasou@gmail.com>
2024-03-04 21:52:56 -0300

    fronted 1

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+
+
+const Dashboard = () => {
+
+
+  const { data: session, status } = useSession();
+
+  const [catsData, setCatsData] = useState<Cat[] | null>(null);
+
+
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+
+  const getCats = async () => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/cats`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            authorization: `Bearer ${session?.user?.token}`,
+        },
+    });
+    const dataResultCats = await res.json();
+    setCatsData(dataResultCats); // Actualiza el estado con los datos obtenidos    
+  };
+
+  let msg = '';
+  if (session?.user.role === 'admin')
+    msg = 'hola admin';
+  else (
+    msg = 'hola user'
+  )
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <pre>
+        <code>{JSON.stringify(session, null, 2)}</code>
+      </pre>
+      <h2>correo {session?.user.email} </h2>
+      <h2>nivel {session?.user.role} </h2>
+      <h1> {msg} </h1>
+      <button onClick={getCats} className="btn btn-primary">Get Cats</button>
+      <div>
+        {catsData && (
+          <ul>
+            {catsData.map(cat => (
+              <li key={cat.id}>Nombre: {cat.name} -- Edad: {cat.age} </li> // Renderiza el nombre de cada gato
+            ))}
+          </ul>
+        )}
+      </div>
+
+    </div>
+    
+  );
+};
+export default Dashboard;
